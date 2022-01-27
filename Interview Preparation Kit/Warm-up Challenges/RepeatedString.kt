565812a6338c75cba5cd374c4644515e7e797b24
Emmanuel Costa <82117565+Emmanuel-Costa@users.noreply.github.com>
2022-01-27 10:53:41 -0300

    Initial commit, creates first directory with the warm-up challenges

---
+++ Interview Preparation Kit/Warm-up Challenges/RepeatedString.kt
@@ -0,0 +1,65 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+fun repeatedString(s: String, n: Long): Long {
+    // Write your code here
+    var stringA = 0
+    var a: Long = 0
+    for (i in s){
+        if (i == 'a'){
+            stringA += 1
+        }
+    }
+    val stringSize = s.toCharArray().size.toLong()
+    if (n % stringSize == 0L){
+        val m = n/stringSize
+        a = stringA * m
+    }else{
+        val rest = n % stringSize
+        val last = s.dropLast((stringSize - rest).toInt())
+        var remainder = 0L
+        for (i in last){
+            if (i == 'a'){
+                remainder += 1
+            }
+        }
+        val m = (n/stringSize)
+        a = (stringA * m) + remainder
+    }
+    return a
+}
+
+fun main(args: Array<String>) {
+    val s = readLine()!!
+
+    val n = readLine()!!.trim().toLong()
+
+    val result = repeatedString(s, n)
+
+    println(result)
+}
---
+++ Interview Preparation Kit/Warm-up Challenges/SalesByMatch.kt
@@ -0,0 +1,47 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    // Write your code here
+    return ar.groupBy{ it }
+        .values
+        .map{ it.size/2 }
+        .sum()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().trim().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
+}
